94ed794a62a5cff8a7ad6aa093b543ce50e34cca
harshkumar01117 <152732598+harshkumar01117@users.noreply.github.com>
2024-06-19 01:14:18 +0530

    Time: 32 ms (23.50%), Space: 13 MB (26.97%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        // M2
+        stack < pair < char, int >> st;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (st.empty())
+                st.push({s[i], 1});
+            else {
+                if (st.top().first == s[i]) {
+                    int c = st.top().second + 1;
+                    st.pop();
+                    st.push({s[i], c});
+                } else {
+                    st.push({s[i], 1});
+                }
+                if (!st.empty() && st.top().second == k) {
+                    st.pop();
+                }
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            char ch= st.top().first;
+            int in=st.top().second;
+            while(in--){
+                ans.push_back(ch);
+            }
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+
+        // M1 (BUT MEMORY LIMIT EXCEEDED)
+        // bool islastK_1same(char ch,string ans,int q ){
+        //     int n=ans.size();
+        //     for(int i=n-q;i<n;i++){
+        //       if(ans[i]!=ch) return false;
+        //     }
+        //     return true;
+        // }
+
+        //     string removeDuplicates(string s, int k) {
+        //         string ans="";
+        //         for(int i=0;i<s.size();i++){
+        //             char ch=s[i];
+        //             if(ans.size()<k-1){
+        //                 ans.push_back(ch);
+        //             }
+        //             else{
+        //                 if(islastK_1same(ch,ans,k-1)){
+        //                     int l=k-1;
+        //                     while(l>0){
+        //                         ans.pop_back();
+        //                         l--;
+        //                     }
+        //                 }
+        //                 else{
+        //                     ans.push_back(ch);
+        //                 }
+        //             }
+        //     }
+        //     return ans;
+        //     }
+    }
+};
