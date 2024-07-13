3f386e1fc83a335e54fbb460d7dd43c5e42a14ac
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-07-13 16:08:02 +0530

    Time: 16 ms (66.97%), Space: 27.3 MB (21.37%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
+    void isSolve(vector<char>& s, int st, int e) {
+        if (st > e) {
+            return;
+        }
+        swap(s[st], s[e]);
+        isSolve(s, st + 1, e - 1);
+    }
     void reverseString(vector<char>& s) {
 
-reverse(s.begin(),s.end());
+        // M1
+        // reverse(s.begin(),s.end());
+
+        // M2 (Recursion)
+
+        int st = 0;
+        int e = s.size();
+
+        isSolve(s, st, e-1);
 
     }
 };
