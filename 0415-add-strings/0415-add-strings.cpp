f436994703e0fb34116bf56789def92fc25edf7a
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-07-13 17:31:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (47.27%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    string add(string num1, string num2, int p1, int p2, int carry) {
+    void add(string& num1, string& num2, int p1, int p2, int carry,
+             string& ans) {
         // base case
         if (p1 < 0 && p2 < 0) {
             if (carry != 0) {
-                return string(1, carry + '0');
-            } else {
-                return "";
+                ans.push_back(carry + '0');
             }
+            return;
         }
 
         // ek case sove
@@ -16,17 +16,17 @@ public:
         int currsum = n1 + n2 + carry;
         int digit = currsum % 10;
         carry = currsum / 10;
-        string ans = "";
+
         ans.push_back(digit + '0');
-        ans = ans + add(num1, num2, p1 - 1, p2 - 1, carry);
-        return ans;
+        add(num1, num2, p1 - 1, p2 - 1, carry, ans);
     }
 
     string addStrings(string num1, string num2) {
         int p1 = num1.size() - 1;
         int p2 = num2.size() - 1;
         int carry = 0;
-        string ans = add(num1, num2, p1, p2, carry);
+        string ans = "";
+        add(num1, num2, p1, p2, carry, ans);
         reverse(ans.begin(), ans.end());
         return ans;
     }
