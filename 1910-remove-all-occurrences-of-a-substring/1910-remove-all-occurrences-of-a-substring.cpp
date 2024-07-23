47d0b14858fc945b04060411dc11f2b299281322
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-07-23 22:17:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.09%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
+    void solve(string& s, string& part) {
+
+        if (s.find(part) == -1) {
+            return;
+        }
+        if (s.find(part) != -1) {
+            s.erase(s.find(part), part.length());
+        }
+
+        solve(s, part);
+    }
+
     string removeOccurrences(string s, string part) {
-         
-     while(s.find(part)!=-1){
-       s.erase(s.find(part),part.length());
-     }
-     return s;
+
+        // m1
+        //  while(s.find(part)!=-1){
+        //    s.erase(s.find(part),part.length());
+        //  }
+        //  return s;
+
+        solve(s, part);
+        return s;
     }
 };
