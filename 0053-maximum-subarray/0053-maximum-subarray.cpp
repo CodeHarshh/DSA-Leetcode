b535e29f35ec50a19bfdb913e799d5bfc0417fae
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-07-28 19:32:11 +0530

    Time: 76 ms (62.97%), Space: 70.6 MB (10.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     // Famous kaden's algo
     int maxSubArray(vector<int>& nums) {
- 
-    int sum=0;
-    int maxsum=nums[0];
-    for(int i=0;i<nums.size();i++){
-    sum=sum+nums[i];
-        maxsum=max(maxsum,sum);
-    if(sum<0) sum=0;
-    
-    }
-     return maxsum;
+        if(nums.size()==1)return nums[0];
+   int sum=0,maxsum=nums[0];
+   for(int i=0;i<nums.size();i++){
+    sum+=nums[i];
+    maxsum=max(maxsum,sum);
+      if(sum<0) sum=0;
+   }
+   return maxsum;
     }
 };
