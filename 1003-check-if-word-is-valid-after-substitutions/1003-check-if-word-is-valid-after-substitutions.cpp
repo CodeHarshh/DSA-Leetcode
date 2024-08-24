d9b6a2b6a6f2b8c6f02b275ca708b8251cae1a52
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-08-24 23:40:17 +0530

    Time: 7 ms (91.51%), Space: 10.9 MB (34.37%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size()==0) return true;
-        
-        int find=s.find("abc");
-        if(find!=string::npos){ //found
-            string left=s.substr(0,find);
-             string right=s.substr(find+3,s.size());
-             return isValid(left+right);
+        if (s[0] == 'b' || s[0] == 'c')
+            return false;
+
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == 'a') {
+                st.push('a');
+                 }
+            else if (ch == 'b'){
+                if (!st.empty() && st.top() == 'a') st.push('b');
+                else return false;
+            } else if (ch == 'c') { // it will be c
+                if (!st.empty() && st.top() == 'b') {
+                    st.pop();
+                    if (!st.empty() && st.top() == 'a') {
+                        st.pop();
+                    } else
+                        return false;
+                } else
+                    return false;
+            }
         }
-        return false;
+        return st.empty();
     }
+    // m2
+    // bool isValid(string s) {
+    //     if(s.size()==0) return true;
+
+    //     int find=s.find("abc");
+    //     if(find!=string::npos){ //found
+    //         string left=s.substr(0,find);
+    //          string right=s.substr(find+3,s.size());
+    //          return isValid(left+right);
+    //     }
+    //     return false;
+    // }
 };
