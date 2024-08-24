5415dfa8330b7652dad8f1b768cfe30faa4aa4af
Harsh Kumar <152732598+harshkumar01117@users.noreply.github.com>
2024-08-25 01:09:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (22.33%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        for (char it : s) {
+
+            if (it == ']') {
+                string temp = "";
+                while (!st.empty() && st.top() != "[") {
+
+                    temp = st.top() + temp;
+                    st.pop();
+                }
+                if (!st.empty() && st.top() == "[") { // Pop the '['
+                    st.pop();
+                }
+
+                string NumbericString = "";
+                while (!st.empty() && isdigit(st.top()[0])) {
+                    NumbericString = st.top() + NumbericString;
+                    st.pop();
+                }
+
+                int cnt = 0;
+                for (char c : NumbericString) {
+                    if (isdigit(c)) {
+                        cnt = cnt * 10 + (c - '0');
+                    }
+                }
+
+                string repeatedString = "";
+                for (int i = 0; i < cnt; i++) {
+                    repeatedString = repeatedString + temp;
+                }
+                st.push(repeatedString);
+
+            } else {
+                string temp1 = string(1, it); // char converted into string
+                st.push(temp1);
+            }
+        }
+
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
+    }
+};
