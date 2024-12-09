5d7d0a099d9c6c9f3038083b0778561b2e9200b1
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2024-12-09 20:02:03 +0530

    Time: 0 ms (100%), Space: 22.9 MB (17.89%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,25 +1,45 @@
 
 class Solution {
+    // public:
+    ///////// m1 - o(n^2)
+    // int getHeight(TreeNode* root) {
+    //     if (root == NULL) return 0;
+    //     int lh = getHeight(root->left);
+    //     int rh = getHeight(root->right);
+    //     return max(lh, rh) + 1;
+    // }
+
+    // bool isBalanced(TreeNode* root) {
+
+    //     if (root == NULL) return true;
+
+    //     int lh = getHeight(root->left);
+    //     int rh = getHeight(root->right);
+
+    //     int diff = abs(lh - rh);
+    //     bool status = diff <= 1;
+
+    //     return status&& isBalanced(root->left) && isBalanced(root->right);
+    // }
+
+    ///// m2 o(N)
+
 public:
+    bool isbalanced = true;
+
     int getHeight(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+        if (root == NULL) return 0;
         int lh = getHeight(root->left);
         int rh = getHeight(root->right);
-        int maxHeight = max(lh, rh);
-        return maxHeight + 1;
+
+        if (isbalanced && abs(lh - rh) > 1) {
+            isbalanced = false;
+        }
+        return max(lh, rh) + 1;
     }
     bool isBalanced(TreeNode* root) {
 
-        if (root == NULL)
-            return true;
-
-        int lh = getHeight(root->left);
-        int rh = getHeight(root->right);
-
-        int diff = abs(lh - rh);
-        bool status = diff <= 1;
-
-        return status&& isBalanced(root->left) && isBalanced(root->right);
+        getHeight(root);
+        return isbalanced;
     }
 };
