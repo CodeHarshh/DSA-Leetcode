9a20c3697cbb9015a69ecfe5771837f74270855c
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2024-12-12 22:58:36 +0530

    Time: 0 ms (100%), Space: 10.7 MB (30.01%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,54 @@
+
+class Solution {
+public:
+    // tc->o(N) , sc->o(N)
+    // void solve(TreeNode* root, vector<int>& ans) {
+    //     if (root == NULL)
+    //         return;
+
+    //     solve(root->left, ans);
+    //     ans.push_back(root->val);
+    //     solve(root->right, ans);
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     solve(root, ans);
+    //     return ans;
+    // }
+
+
+
+
+    // by using morries traversal tc->o(N) , sc->o(1)
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
+
+        while (curr) {
+            // left node is null , then visit it and go write
+
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr=curr->right;
+            } else { // find inorder predecessor
+                // ek baar left then right right right..
+                TreeNode* pred = curr->left;
+
+                 while (pred->right != NULL && pred->right != curr) {
+                pred = pred->right;
+            }
+
+                if (pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
