4f8dfee209604a727915daca9fa2f22b52a1bee5
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2024-12-15 11:01:14 +0530

    Time: 0 ms (100%), Space: 37 MB (58.19%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    void inorder(vector<int>& v, TreeNode* root) {
+        if (!root)  return;
+        inorder(v, root ->left);
+        v.push_back(root->val);
+        inorder(v, root->right);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> v;
+        inorder(v, root);
+        int i = 0, j = v.size()-1;
+        while (i < j) {
+            if (v[i] + v[j] == k) return true;
+            else if (v[i] + v[j] < k) i++;
+            else if (v[i] + v[j] > k) j--;
+        }
+        return false;
+    }
+};
