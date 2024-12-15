2b1a5ccfa270273e3e54017c132b6874de07d670
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2024-12-15 12:02:18 +0530

    Time: 0 ms (100%), Space: 11.3 MB (23.62%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    void solve(TreeNode*& root, int& sum) {
+        if (!root) return;
+        solve(root->right, sum);
+        sum = sum + root->val;
+        root->val = sum;
+        solve(root->left, sum);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        solve(root, sum);
+        return root;
+    }
+};
