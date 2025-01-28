35a9291af1b34c9ccda4e017a19d1d7f5aeb4e7f
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-01-29 00:22:08 +0530

    Time: 0 ms (100%), Space: 60.9 MB (61.96%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+//only two noeds are swapped by mistake
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+
+
+    void inorder(TreeNode* &root){
+    if(!root)return;
+    inorder(root->left);
+
+    if(prev!= NULL && (root->val < prev->val)){ // mean voilation occurs
+        if(first==NULL){ // first time voilation
+        first=prev;
+        middle=root;
+        }
+
+        else{ // second time voilation
+            last=root;
+        }
+    }
+        prev=root;
+
+    inorder(root->right);
+    }
+  
+    void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+      if(first && last )swap(first->val,last->val);
+      else if(first && middle)swap(first->val,middle->val);
+
+    }
+};
