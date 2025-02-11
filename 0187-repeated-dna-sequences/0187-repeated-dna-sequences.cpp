b55675a62ade22da3f6fd8f7bc3957999a5669ce
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-02-12 00:50:40 +0530

    Time: 53 ms (23.92%), Space: 26.2 MB (45.69%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -8,10 +8,13 @@ public:
         string current=s.substr(i,10);
           m[current]++;
         }
-        for(auto & it:m){
+        for(auto it:m){
             if(it.second>1){
                 ans.push_back(it.first);
             }
+        }
+        for(auto it:ans){
+            cout<<it<<" ";
         }
         return ans;
     }
