cf025367fad0d4a1f01499383e2702cb40617e53
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-04-13 23:53:45 +0530

    Time: 67 ms (70.27%), Space: 15.5 MB (61.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -32,7 +32,7 @@ public:
 
 
 // DP MEthod-1 
-// bool solve(vector<int>& nums, int target, int i, vector<vector<int>>&dp) {
+//bool solve(vector<int>& nums, int target, int i, vector<vector<int>>&dp) {
 //         if (target < 0 || i == nums.size()) return false;
 //         if (target == 0) return true;
 
@@ -56,28 +56,64 @@ public:
 
 // Methos -2 using tabulation 
 
-bool solveusingTabulation(vector<int>& nums, int target, int i) {
+//bool solveusingTabulation(vector<int>& nums, int target, int i) {
+//int n=nums.size();
+//      vector<vector<bool>>dp(target+1,vector<bool>(n+1,0));
+
+//         for(int col=0;col<=nums.size();col++){ // if (target == 0) return true;  zero th row me sb ko true set krna ha;
+//             dp[0][col]=true;
+//         }
+
+//      for (int t = 1; t <= target; t++) {
+//         for (int i = n - 1; i >= 0; i--) {
+//            bool inc = 0;
+//             if(t - nums[i] >=0){
+//                 inc=dp[t - nums[i]][i + 1];
+//             }
+//         bool exc = dp[t][i + 1];
+//         dp[t][i]=inc || exc;
+//             }
+//         }
+        
+//         return dp[target][0];
+//     }
+
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(), nums.end(),0);
+//         if (sum % 2 != 0) return false;
+      
+//         int target = sum / 2;
+       
+//         return solveusingTabulation(nums,target,0);
+//     }
+
+
+// mehtod 3 (space optimization)
+
+bool Optimization(vector<int>& nums, int target, int i) {
   int n=nums.size();
-     vector<vector<bool>>dp(target+1,vector<bool>(n+1,0));
-
-
-        
-        
+    vector<int>curr(target+1,0);
+    vector<int>next(target+1,0);
 
         for(int col=0;col<=nums.size();col++){ // if (target == 0) return true;  zero th row me sb ko true set krna ha;
-            dp[0][col]=true;
+            next[0]=true;
         }
 
-     for (int t = 1; t <= target; t++) {
+    
         for (int i = n - 1; i >= 0; i--) {
-
-       bool inc = (t >= nums[i]) ? dp[t - nums[i]][i + 1] : false;
-        bool exc = dp[t][i + 1];
-        dp[t][i]=inc || exc;
+             for (int t = 1; t <= target; t++) {
+           bool inc = 0;
+            if(t - nums[i] >=0){
+                inc=next[t - nums[i]];
             }
+        bool exc = next[t];
+        curr[t]=inc || exc;
+       
+
+            } next=curr;
         }
         
-        return dp[target][0];
+        return next[target];
     }
 
     bool canPartition(vector<int>& nums) {
@@ -86,6 +122,6 @@ bool solveusingTabulation(vector<int>& nums, int target, int i) {
       
         int target = sum / 2;
        
-        return solveusingTabulation(nums,target,0);
+        return Optimization(nums,target,0);
     }
 };
