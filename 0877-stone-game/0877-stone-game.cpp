b83de9d096fd123974bb68e04c0b44cc6f492e62
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-05-23 23:24:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (56.43%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
-void solve(vector<int>& piles,int i,int j,int alice,int bob){
-     
-    // if(piles[i]>piles[j]){
-    //     solve(piles,i+1,j,(alice+piles[i],bob); 
-    //     solve(piles,)
-    // }
-    // else{
-
-    // }
-}
+    // alice will always win
     bool stoneGame(vector<int>& piles) {
-    //   int i=0;
-    //   int j=piles.size()-1;
-    //   solve(piles,i,j,0,0);
         return true;
     }
 };
