7aca053da4ed7eb57bf0233e466103f8c655a153
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-05-27 20:57:45 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.97%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+// dp assingment
+////////////// Brute force method  TLE    
+    // int solve(vector<int>& days, vector<int>& costs, int i) {
+    //     // Base case: No more days left
+    //     if (i >= days.size()) return 0;
+
+    //     // Option 1: Buy 1-day pass & move to next day
+    //     int cost1 = costs[0] + solve(days, costs, i + 1);
+
+    //     // Option 2: Buy 7-day pass & skip covered days
+    //     int passEndDay = days[i] + 7 - 1;
+    //     int j = i;
+    //     while (j < days.size() && days[j] <= passEndDay) j++;
+    //     int cost7 = costs[1] + solve(days, costs, j);
+
+    //     // Option 3: Buy 30-day pass & skip covered days
+    //     passEndDay = days[i] + 30 - 1;
+    //     j = i;
+    //     while (j < days.size() && days[j] <= passEndDay) j++;
+    //     int cost30 = costs[2] + solve(days, costs, j);
+
+    //     // Return the minimum of all options
+    //     return min(cost1, min(cost7, cost30));
+    // }
+
+    //     int mincostTickets(vector<int>& days, vector<int>& costs) {
+    //         return solve(days, costs, 0);
+    //     }
+
+
+
+/////////// memoization method / Dp method- 1
+int solve(vector<int>& days, vector<int>& costs, int i,vector<int>&dp) {
+    // Base case: No more days left
+    if (i >= days.size()) return 0;
+
+    if(dp[i]!=-1) return dp[i];
+
+    // Option 1: Buy 1-day pass & move to next day
+    int cost1 = costs[0] + solve(days, costs, i + 1,dp);
+
+    // Option 2: Buy 7-day pass & skip covered days
+    int passEndDay = days[i] + 7 - 1;
+    int j = i;
+    while (j < days.size() && days[j] <= passEndDay) j++;
+    int cost7 = costs[1] + solve(days, costs, j,dp);
+
+    // Option 3: Buy 30-day pass & skip covered days
+    passEndDay = days[i] + 30 - 1;
+    j = i;
+    while (j < days.size() && days[j] <= passEndDay) j++;
+    int cost30 = costs[2] + solve(days, costs, j,dp);
+
+    // Return the minimum of all options
+    return dp[i]=min(cost1, min(cost7, cost30));
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int>dp(days.size()+1,-1);
+        return solve(days, costs, 0,dp);
+    }
+
+};
