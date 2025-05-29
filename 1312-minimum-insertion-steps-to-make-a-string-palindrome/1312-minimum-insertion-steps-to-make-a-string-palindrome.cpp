f5b5af44375d02fa1e3ff5cbb1a3ea8a40cbcf95
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-05-29 23:33:37 +0530

    Time: 34 ms (55.25%), Space: 31.5 MB (58.99%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int solve(string& s, int i, int j, vector<vector<int>>& dp) {
+        if (i > j)return 0;
+        if (i == j)return 1;
+        int ans;
+
+        if (dp[i][j] != -1)return dp[i][j];
+
+        if (s[i] == s[j]) {
+            ans = 2 + solve(s, i + 1, j - 1, dp);
+        } else {
+            ans = max(solve(s, i + 1, j, dp), solve(s, i, j - 1, dp));
+        }
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int LongestPalindrome= solve(s, 0, n - 1, dp);
+        return s.size()-LongestPalindrome;
+    }
+};
