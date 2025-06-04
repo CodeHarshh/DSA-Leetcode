c602830e2141a8c13a4367655646c2a9e831ae14
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-06-04 22:26:02 +0530

    Time: 19 ms (50.67%), Space: 40.9 MB (22.31%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    int cnt;
-    void solve(vector<int>& nums, int target, int i, long long sum) {
-        if (i == nums.size()) {
-            if (sum == target) {
-                cnt++;
-            }
-            return;
-        }
+    // int cnt;
+    // void solve(vector<int>& nums, int target, int i, long long sum) {
+    //     if (i == nums.size()) {
+    //         if (sum == target) {
+    //             cnt++;
+    //         }
+    //         return;
+    //     }
 
-        solve(nums, target, i + 1, sum + nums[i]);
-        solve(nums, target, i + 1, sum - nums[i]);
+    //     solve(nums, target, i + 1, sum + nums[i]);
+    //     solve(nums, target, i + 1, sum - nums[i]);
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     cnt = 0;
+    //     solve(nums, target, 0, 0);
+    //     return cnt;
+    // }
+
+int solve(vector<int>& nums, int target, int i, int sum, vector<vector<int>>& dp) {
+        if (i == nums.size()) {
+            return (sum == target) ? 1 : 0;
+        }
+        if (dp[i][sum + 1000] != -1) {   ///+1000 use to handle -ve case
+            return dp[i][sum + 1000];
+        }
+        int add = solve(nums, target, i + 1, sum + nums[i], dp);
+        int subtract = solve(nums, target, i + 1, sum - nums[i], dp);
+        return dp[i][sum + 1000] = add + subtract;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        cnt = 0;
-        solve(nums, target, 0, 0);
-        return cnt;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2001, -1));
+        return solve(nums, target, 0, 0, dp);
     }
+
 };
