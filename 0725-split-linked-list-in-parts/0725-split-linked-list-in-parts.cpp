a8b7964e5d92d64b3dda9ec12495e9a3224d2724
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-04 04:11:33 +0530

    Time: 0 ms (100%), Space: 13.6 MB (50.68%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -1,40 +1,60 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
         int n = 0;
+        vector<ListNode*> ans(k, NULL);
         ListNode* temp = head;
         while (temp != NULL) {
             n++;
             temp = temp->next;
         }
+        int eachPartSize = n / k;
+        int extraSize = n % k;
 
-        int partSize = n / k;
-        int ExtraSize = n % k;
-        vector<ListNode*> ans(k, NULL);
         temp = head;
-
         for (int i = 0; i < k; i++) {
             ans[i] = temp;
-            int currPartSize = partSize + (ExtraSize-- > 0 ? 1 : 0);
-            for (int j = 0; j < currPartSize - 1; j++) {
+            int currPartSize = eachPartSize + (extraSize-- > 0 ? 1 : 0);
+
+            for (int i = 0; i < currPartSize - 1; i++) {
                 temp = temp->next;
             }
             if (temp != NULL) {
-                ListNode* NextPartStart = temp->next;
+                ListNode* next = temp->next;
                 temp->next = NULL;
-                temp = NextPartStart;
+                temp = next;
             }
         }
         return ans;
     }
+
+    //  vector<ListNode*> splitListToParts(ListNode* head, int k) {
+    //     int n = 0;
+    //     ListNode* temp = head;
+    //     while (temp != NULL) {
+    //         n++;
+    //         temp = temp->next;
+    //     }
+
+    //     int partSize = n / k;
+    //     int ExtraSize = n % k;
+    //     vector<ListNode*> ans(k, NULL);
+    //     temp = head;
+
+    //     for (int i = 0; i < k; i++) {
+    //         ans[i] = temp;
+    //         // Add 1 extra node to this part if any extras are left
+    //         int currPartSize = partSize + (ExtraSize-- > 0 ? 1 : 0);
+    //         for (int j = 0; j < currPartSize - 1; j++) {
+    //             temp = temp->next;
+    //         }
+    //         if (temp != NULL) {
+    //             ListNode* NextPartStart = temp->next;
+    //             temp->next = NULL;
+    //             temp = NextPartStart;
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
