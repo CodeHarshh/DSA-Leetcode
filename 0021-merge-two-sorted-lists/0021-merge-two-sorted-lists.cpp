7b865b6e6bd2f33c8645ce7a1a1cfd9217678c05
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-04 04:44:29 +0530

    Time: 0 ms (100%), Space: 19.4 MB (87.1%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,36 +1,64 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+    if(left==NULL)return right;
+    if(right==NULL)return left;
+    
+    ListNode* dummy=new ListNode(-1);
+    ListNode* temp=dummy;
 
-        if (!left) return right;
-        if (!right) return left;
-
-        ListNode* ans = new ListNode(-1);
-        ListNode* mptr = ans;
-
-        while (left && right) {
-            if (left->val <= right->val) {
-                mptr->next = left;
-                mptr = left;
-                left = left->next;
-            } else {
-                mptr->next = right;
-                mptr = right;
-                right = right->next;
-            }
+    while(left && right){
+        if(left->val <= right->val){
+         temp->next=left;
+         temp=left;
+         left=left->next;
         }
-        if (left) mptr->next = left;
-        if (right) mptr->next = right;
-        return ans->next;
+
+        else if(right->val <= left->val){
+         temp->next=right;
+         temp=right;
+         right=right->next;
+        }
     }
+
+    if(left) temp->next=left;
+    if(right) temp->next=right;
+
+    return dummy->next;
+   
+    }
+
+
+
+
+
+
+
+
+
+
+    //    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+
+    //     if (!left) return right;
+    //     if (!right) return left;
+
+    //     ListNode* ans = new ListNode(-1);
+    //     ListNode* mptr = ans;
+
+    //     while (left && right) {
+    //         if (left->val <= right->val) {
+    //             mptr->next = left;
+    //             mptr = left;
+    //             left = left->next;
+    //         } else {
+    //             mptr->next = right;
+    //             mptr = right;
+    //             right = right->next;
+    //         }
+    //     }
+    //     if (left) mptr->next = left;
+    //     if (right) mptr->next = right;
+    //     return ans->next;
+    // }
 };
