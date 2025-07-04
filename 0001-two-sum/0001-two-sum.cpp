637861ad97f8f19b96cf8aa74668ace215d4f4f5
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-05 00:05:49 +0530

    Time: 0 ms (100%), Space: 14.6 MB (59.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -15,23 +15,45 @@ public:
     //     return ans;
     // }
 
-
     // m2 ->n(log)n
 
-        vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> ans; // value+index
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     vector<pair<int, int>> ans; // value+index
 
-        for(int i=0;i<nums.size();i++ ){
-            ans.push_back({nums[i],i});
-        }
-        sort(ans.begin(),ans.end());
-      int i=0,j=nums.size()-1;
-      while(i<j){
-      if(ans[i].first+ans[j].first==target)return{ans[i].second,ans[j].second};
-      else if(ans[i].first+ans[j].first>target)j--;
-      else if(ans[i].first+ans[j].first<target)i++;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         ans.push_back({nums[i], i});
+    //     }
+    //     sort(ans.begin(), ans.end());
+    //     int i = 0, j = nums.size() - 1;
+    //     while (i < j) {
+    //         if (ans[i].first + ans[j].first == target)return {ans[i].second, ans[j].second};
+    //         else if (ans[i].first + ans[j].first > target)j--;
+    //         else if (ans[i].first + ans[j].first < target)i++;
+    //     }
+    //     return {-1, -1};
+    // }
+
+
+
+
+       vector<int> twoSum(vector<int>& nums, int target) {
+      vector<pair<int,int>>v;
+      for(int i=0;i<nums.size();i++){
+        v.push_back({nums[i],i});
       }
-       
-        return {-1,-1};
+      sort(v.begin(),v.end()); // -> tc: N log(N)
+     int i=0,j=nums.size()-1;
+     while(i<j){
+        if(v[i].first+v[j].first==target){
+            return{v[i].second,v[j].second};
+        }
+        else if(v[i].first+v[j].first>target){
+           j--;
+        }
+          else if(v[i].first+v[j].first<target){
+           i++;
+        }
+     }
+  return {-1,-1};
     }
 };
