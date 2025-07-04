897bf30f8f1b030e2c2be790ccf2e3b3d608594a
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-04 16:33:39 +0530

    Time: 7 ms (18.07%), Space: 11.7 MB (73.84%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -1,44 +1,83 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-};
-*/
-
 class Solution {
 public:
     Node* solve(Node* head) {
-        if (head == NULL)
-            return NULL;
-        Node* start = head;
-        Node* tail = start;
+        if (head == NULL)return NULL;
 
-        while (start != NULL) {
-            if (start->child) {
-                Node* childtail = solve(start->child);
+        Node* curr = head;
 
-                // flatten's work
-                Node* temp = start->next;
-                start->next = start->child;
-                start->next->prev = start;
-                childtail->next = temp;
-                if (temp != NULL) {
-                    temp->prev = childtail;
+        while (curr != NULL) {
+            if (curr->child != NULL) {
+                Node* frwd = curr->next;
+
+                Node* Flatten = solve(curr->child);
+                curr->next = Flatten;
+                curr->next->prev = curr; // double L.L
+                curr->child = NULL;
+
+                while (curr->next != NULL) {
+                    curr = curr->next;
                 }
-                start->child = NULL;
+                if (frwd != NULL) {
+                    curr->next = frwd;
+                    frwd->prev = curr;
+                }
             }
-            tail = start;
-            start = start->next;
+            curr = curr->next;
         }
-        return tail;
+        return head;
     }
 
     Node* flatten(Node* head) {
         solve(head);
         return head;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Node* solve(Node* head) {
+    //     if (head == NULL)
+    //         return NULL;
+    //     Node* start = head;
+    //     Node* tail = start;
+
+    //     while (start != NULL) {
+    //         if (start->child) {
+    //             Node* childtail = solve(start->child);
+
+    //             // flatten's work
+    //             Node* temp = start->next;
+    //             start->next = start->child;
+    //             start->next->prev = start;
+    //             childtail->next = temp;
+    //             if (temp != NULL) {
+    //                 temp->prev = childtail;
+    //             }
+    //             start->child = NULL;
+    //         }
+    //         tail = start;
+    //         start = start->next;
+    //     }
+    //     return tail;
+    // }
+
+    // Node* flatten(Node* head) {
+    //     solve(head);
+    //     return head;
+    // }
 };
