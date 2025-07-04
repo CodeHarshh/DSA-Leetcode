1661c0b03946d40faa46a384f7d5acc59eecc212
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-04 23:20:56 +0530

    Time: 0 ms (100%), Space: 16.4 MB (64.53%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     int getLen(ListNode* head) {
-        ListNode *temp = head;
+        ListNode* temp = head;
         int len = 0;
         while (temp) {
             len++;
@@ -20,28 +20,61 @@ public:
         return len;
     }
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head) return NULL;
-        int len=getLen(head);
-        int actualrotate=(k%len);
-        if(actualrotate==0) return head;
-        int newlastnodepos=len-actualrotate-1;
+        if (head == NULL) return NULL;
 
-        ListNode* newnodehead=NULL;
-        ListNode* newLastnode=head;
-        for(int i=0;i<newlastnodepos;i++){
-            newLastnode=newLastnode->next;
+        int len = getLen(head);
+         k = k % len;
+         if(k==0) return head;
+        int Pos = len - k - 1;
+
+        ListNode* temp = head;
+        for (int i = 0; i < Pos && temp != NULL; i++) {
+            temp = temp->next;
         }
-        newnodehead=newLastnode->next;
+          ListNode* newHead = temp->next;
+          temp->next=NULL;
 
-        newLastnode->next=NULL;
-        
-        ListNode* it=newnodehead;
-        while(it->next){
-            it=it->next;
-        }
-        it->next=head;
-        return newnodehead;
+            ListNode* temp2 = newHead;
 
-
+          while(temp2->next!=NULL){
+             temp2=temp2->next;
+          }
+          temp2->next=head;
+          head=newHead;
+      return head;
     }
+
+    // int getLen(ListNode* head) {
+    //     ListNode *temp = head;
+    //     int len = 0;
+    //     while (temp) {
+    //         len++;
+    //         temp = temp->next;
+    //     }
+    //     return len;
+    // }
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if (!head) return NULL;
+    //     int len=getLen(head);
+    //     int actualrotate=(k%len);
+    //     if(actualrotate==0) return head;
+    //     int newlastnodepos=len-actualrotate-1;
+
+    //     ListNode* newnodehead=NULL;
+    //     ListNode* newLastnode=head;
+    //     for(int i=0;i<newlastnodepos;i++){
+    //         newLastnode=newLastnode->next;
+    //     }
+    //     newnodehead=newLastnode->next;
+
+    //     newLastnode->next=NULL;
+
+    //     ListNode* it=newnodehead;
+    //     while(it->next){
+    //         it=it->next;
+    //     }
+    //     it->next=head;
+    //     return newnodehead;
+
+    // }
 };
