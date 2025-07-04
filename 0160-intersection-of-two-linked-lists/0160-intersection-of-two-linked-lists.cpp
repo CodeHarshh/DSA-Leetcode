4019a2810a3aea3b765123c9691e443a765593e4
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-04 17:40:57 +0530

    Time: 37 ms (61.27%), Space: 18.6 MB (52.86%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,44 +1,79 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-        int cnt1 = 0, cnt2 = 0;
-        while (temp1) {
-            cnt1++;
-            temp1 = temp1->next;
+        ListNode* a = headA;
+        ListNode* b = headB;
+        int cntA = 0, cntB = 0;
+
+        while (a != NULL) {
+            cntA++;
+            a = a->next;
         }
-        while (temp2) {
-            cnt2++;
-            temp2 = temp2->next;
+        while (b != NULL) {
+            cntB++;
+            b = b->next;
         }
-        temp1 = headA;
-        temp2 = headB;
-        int k = abs(cnt1 - cnt2);
-        if (cnt1 > cnt2) {
-            for (int i = 0; i < k; i++) {
-                temp1 = temp1->next;
+        int k = abs(cntA - cntB);
+        a = headA;
+        b = headB;
+
+            if (cntA > cntB) {
+                for (int i = 0; i < k; i++) {
+                    a = a->next;
+                }
+            } else if (cntB > cntA) {
+                for (int i = 0; i < k; i++) {
+                    b = b->next;
+                }
             }
-        } else {
-            for (int i = 0; i < k; i++) {
-                temp2 = temp2->next;
-            }
-        }
-        while (temp1 || temp2) {
-            if (temp1 == temp2) {
-                return temp1;
-            }
-            temp1 = temp1->next;
-            temp2 = temp2->next;
+        
+
+        while (a || b) {
+            if (a == b)return a;
+
+            a = a->next;
+            b = b->next;
         }
         return NULL;
     }
+
+
+
+
+
+
+    // ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    //     ListNode* temp1 = headA;
+    //     ListNode* temp2 = headB;
+    //     int cnt1 = 0, cnt2 = 0;
+    //     while (temp1) {
+    //         cnt1++;
+    //         temp1 = temp1->next;
+    //     }
+    //     while (temp2) {
+    //         cnt2++;
+    //         temp2 = temp2->next;
+    //     }
+    //     temp1 = headA;
+    //     temp2 = headB;
+    //     int k = abs(cnt1 - cnt2);
+    //     if (cnt1 > cnt2) {
+    //         for (int i = 0; i < k; i++) {
+    //             temp1 = temp1->next;
+    //         }
+    //     } else {
+    //         for (int i = 0; i < k; i++) {
+    //             temp2 = temp2->next;
+    //         }
+    //     }
+    //     while (temp1 || temp2) {
+    //         if (temp1 == temp2) {
+    //             return temp1;
+    //         }
+    //         temp1 = temp1->next;
+    //         temp2 = temp2->next;
+    //     }
+    //     return NULL;
+    // }
 };
