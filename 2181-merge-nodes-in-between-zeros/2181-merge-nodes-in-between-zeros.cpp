f153184566dbb2012f93924f5c511895566440b4
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-05 18:04:04 +0530

    Time: 64 ms (18.2%), Space: 295.4 MB (19.7%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,33 +1,67 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
-    ListNode* mergeNodes(ListNode* head) {
-        ListNode* newnode = new ListNode(-1);
-        ListNode* temp = newnode;
 
-        int sum = 0;
-        ListNode* it = head->next;
-        while (it != NULL) {
-            if (it->val == 0) {
-                if (sum > 0) {
-                    temp->next = new ListNode(sum);
-                    temp = temp->next;
-                    sum = 0;
-                }
-            } else {
-                sum = sum + it->val;
+    ListNode* mergeNodes(ListNode* head) {
+     if(!head) return NULL;
+     
+     int sum=0;
+
+     ListNode* dummy=new ListNode(-1);
+     ListNode* temp=dummy;
+
+     ListNode* it=head;
+
+     while(it!=NULL){
+        if(it->val==0){
+            if(sum>0){
+                temp->next=new ListNode(sum);
+                temp=temp->next;
+                sum=0;
             }
-            it = it->next;
         }
-        return newnode->next;
+        else{
+            sum=sum+ (it->val);
+        }
+          it=it->next;
+     }
+return dummy->next;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ListNode* mergeNodes(ListNode* head) {
+    //     ListNode* newnode = new ListNode(-1);
+    //     ListNode* temp = newnode;
+
+    //     int sum = 0;
+    //     ListNode* it = head->next;
+    //     while (it != NULL) {
+    //         if (it->val == 0) {
+    //             if (sum > 0) {
+    //                 temp->next = new ListNode(sum);
+    //                 temp = temp->next;
+    //                 sum = 0;
+    //             }
+    //         } else {
+    //             sum = sum + it->val;
+    //         }
+    //         it = it->next;
+    //     }
+    //     return newnode->next;
+    // }
 };
