116308b040e79933110e5971b9a3f178268e91aa
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-09 02:24:15 +0530

    Time: 4 ms (36.12%), Space: 185.3 MB (38.72%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,30 +1,65 @@
 
 class Solution {
 public:
+    //  swapping the values question
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp = head;
+        if (!head) return head;
+
         int cnt = 0;
-        while (temp != NULL) {
+        ListNode* first = head;
+        ListNode* last = head;
+
+        while (first != NULL) {
             cnt++;
-            temp = temp->next;
+            first = first->next;
         }
-        
-        int pos = 1 ,lastPos = cnt - k + 1;
-        temp = head;
-        ListNode* first = NULL;
-        ListNode* last = NULL;
-        while (temp != NULL) {
-            if (pos == k) first = temp;
-            if (pos == lastPos) last = temp;
-            temp = temp->next;
+       
+        first = head;
+        int Lpos=cnt-k+1 ,pos=1;
+
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(pos==k) first=temp;
+            if(pos==Lpos) last=temp;
             pos++;
+            temp=temp->next;
         }
-        if (first && last) {
-            int temp1;
-            temp1 = first->val;
-            first->val = last->val;
-            last->val = temp1;
+
+        if(first&&last){
+         
+            int temp=first->val;
+            first->val=last->val;
+            last->val=temp;
         }
-        return head;
+
+return head;
+
     }
+
+    // ListNode* swapNodes(ListNode* head, int k) {
+    //     ListNode* temp = head;
+    //     int cnt = 0;
+    //     while (temp != NULL) {
+    //         cnt++;
+    //         temp = temp->next;
+    //     }
+
+    //     int pos = 1 ,lastPos = cnt - k + 1;
+    //     temp = head;
+    //     ListNode* first = NULL;
+    //     ListNode* last = NULL;
+    //     while (temp != NULL) {
+    //         if (pos == k) first = temp;
+    //         if (pos == lastPos) last = temp;
+    //         temp = temp->next;
+    //         pos++;
+    //     }
+    //     if (first && last) {
+    //         int temp1;
+    //         temp1 = first->val;
+    //         first->val = last->val;
+    //         last->val = temp1;
+    //     }
+    //     return head;
+    // }
 };
