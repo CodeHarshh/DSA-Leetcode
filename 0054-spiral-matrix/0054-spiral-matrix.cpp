c43470ce9ba8dd205cb9ec9743429718f0f7bbe6
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-09 12:29:20 +0530

    Time: 0 ms (100%), Space: 9.4 MB (49.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,47 +1,100 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>ans;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int total=m*n;
+    vector<int> spiralOrder(vector<vector<int>>& m) {
+        vector<int> ans;
 
-    int StartingColumn=0;
-    int EndingColumn=n-1;
-       int EndingRow=m-1;
-    int StartingRow=0;
+        int row = m.size();
+        int col = m[0].size();
+        int total = row * col;
 
-int cnt=0;
+        int startCol = 0 ;
+        int endCol = col - 1;
 
-while(cnt<total){
+        int startRow = 0;
+        int endRow = row - 1;
 
-for(int i=StartingColumn;i<=EndingColumn  && cnt<total;i++){
-    ans.push_back(matrix[StartingRow][i]);
-cnt++;
+        int cnt = 0;
+        while (cnt < total) {
+
+            for (int i = startCol; i <= endCol && cnt<total ; i++) { // && cnt<total ---> imp
+                ans.push_back(m[startRow][i]);
+                cnt++;
+            }
+            startRow++;
+
+            for (int i = startRow; i <= endRow && cnt<total; i++) {
+                ans.push_back(m[i][endCol]);
+                cnt++;
+            }
+            endCol--;
+
+            for (int i = endCol; i >= startCol && cnt<total; i--) {
+                ans.push_back(m[endRow][i]);
+                cnt++;
+            }
+            endRow--;
+      
+
+            for (int i = endRow; i >= startRow && cnt<total; i--) {
+                ans.push_back(m[i][startCol]);
+                cnt++;
+            }
+            startCol++;
+        }
+   return ans;
+   }
+       
+    };
 
 
-}
-StartingRow++;
 
-for(int i=StartingRow;i<=EndingRow  && cnt<total;i++){
-     ans.push_back(matrix[i][EndingColumn]);
- cnt++;
-}
-EndingColumn--;
 
-for(int i=EndingColumn;i>=StartingColumn  && cnt<total;i--){
-     ans.push_back(matrix[EndingRow][i]);
-  cnt++;
-}
-EndingRow--;
 
-for(int i=EndingRow;i>=StartingRow  && cnt<total ;i--){
-     ans.push_back(matrix[i][StartingColumn]);
- cnt++;
-}
-StartingColumn++;
 
-    }
-return ans;
-    }
-};
+
+
+
+    //     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    //         vector<int>ans;
+    //         int m=matrix.size();
+    //         int n=matrix[0].size();
+    //         int total=m*n;
+
+    //     int StartingColumn=0;
+    //     int EndingColumn=n-1;
+    //        int EndingRow=m-1;
+    //     int StartingRow=0;
+
+    // int cnt=0;
+
+    // while(cnt<total){
+
+    // for(int i=StartingColumn;i<=EndingColumn  && cnt<total;i++){
+    //     ans.push_back(matrix[StartingRow][i]);
+    // cnt++;
+
+    // }
+    // StartingRow++;
+
+    // for(int i=StartingRow;i<=EndingRow  && cnt<total;i++){
+    //      ans.push_back(matrix[i][EndingColumn]);
+    //  cnt++;
+    // }
+    // EndingColumn--;
+
+    // for(int i=EndingColumn;i>=StartingColumn  && cnt<total;i--){
+    //      ans.push_back(matrix[EndingRow][i]);
+    //   cnt++;
+    // }
+    // EndingRow--;
+
+    // for(int i=EndingRow;i>=StartingRow  && cnt<total ;i--){
+    //      ans.push_back(matrix[i][StartingColumn]);
+    //  cnt++;
+    // }
+    // StartingColumn++;
+
+    //     }
+    // return ans;
+    //     }
+// };
