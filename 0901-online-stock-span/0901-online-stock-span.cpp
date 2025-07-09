6604279b83160499ef861c481cc9ab3c00cd60c5
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-09 17:03:28 +0530

    Time: 18 ms (94.85%), Space: 92.1 MB (28.1%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,19 +1,33 @@
 class StockSpanner {
 public:
-stack<pair<int,int>>st;
-    StockSpanner() {
-        
-    }
-    
+    stack<pair<int, int>> st;
+    StockSpanner() {}
+
     int next(int price) {
         int span=1;
-        while(!st.empty()&& price>=st.top().first){
-span=span+st.top().second;
-st.pop();
+
+        while(!st.empty() && price>= st.top().first){
+            span=span+ st.top().second;
+            st.pop();
         }
         st.push({price,span});
         return span;
     }
+
+
+
+
+
+
+    //  int next(int price) {
+    //     int span = 1;
+    //     while (!st.empty() && price >= st.top().first) {
+    //         span = span + st.top().second;
+    //         st.pop();
+    //     }
+    //     st.push({price, span});
+    //     return span;
+    // }
 };
 
 /**
