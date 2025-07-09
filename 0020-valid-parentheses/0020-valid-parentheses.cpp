5994e54101a15a8e39b6ee6c039058e2061af82c
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-09 16:19:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st;
-        for (int i = 0; i < s.size(); i++) {
-            int ch = s[i];
-            if (ch == '[' || ch == '(' || ch == '{') {
-                st.push(ch);
-            } else {
-                if (!st.empty() && st.top() == '[' && ch == ']' ||
-                   !st.empty() &&  st.top() == '{' && ch == '}' ||
-                   !st.empty() &&  st.top() == '(' && ch == ')') {
-                    st.pop();
-                }
-                else{
-                     return false;
-                } 
-            }
-}
 
 
-        return st.empty();
+  bool isValid(string s) {
+    stack<char>st;
+
+    for(int i=0;i<s.size();i++){
+        char ch=s[i];
+
+        if(ch=='['|| ch=='(' || ch=='{' ){
+            st.push(s[i]);
+        }
+        else{
+              if(ch==']' && st.top()=='[' ||ch==')' && st.top()=='(' || ch=='}' && st.top()=='{' ){
+                st.pop();
+              }
+              else{
+                return false;
+              }
+        }
     }
+    return st.empty()?true:false;
+    
+  }
+
+
+    // bool isValid(string s) {
+    //     stack<char> st;
+    //     for (int i = 0; i < s.size(); i++) {
+    //         int ch = s[i];
+    //         if (ch == '[' || ch == '(' || ch == '{') {
+    //             st.push(ch);
+    //         } else {
+    //             if (!st.empty() && st.top() == '[' && ch == ']' ||
+    //                 !st.empty() && st.top() == '{' && ch == '}' ||
+    //                 !st.empty() && st.top() == '(' && ch == ')') {
+    //                 st.pop();
+    //             } else {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+
+    //     return st.empty();
+    // }
 };
