376bac4449aad3c90ad1a9cac7a9ce91c858b09e
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-12 02:57:53 +0530

    Time: 7 ms (40.23%), Space: 12.1 MB (23.57%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -5,37 +5,57 @@ public:
             return false;
 
         stack<char> st;
+        st.push(s[0]);
 
-        for (char ch : s) {
-            if (ch == 'a') {
-                st.push('a');
-                 }
-            else if (ch == 'b'){
-                if (!st.empty() && st.top() == 'a') st.push('b');
-                else return false;
-            } else if (ch == 'c') { // it will be c
-                if (!st.empty() && st.top() == 'b') {
-                    st.pop();
-                    if (!st.empty() && st.top() == 'a') {
-                        st.pop();
-                    } else
-                        return false;
-                } else
+        for (int i = 1; i < s.size(); i++) {
+            char ch=s[i];
+            if(ch=='a') {st.push('a');}
+
+            else if(!st.empty() && ch=='b' ){
+                if(st.top()=='a'){
+                     st.push('b');
+                }else{
                     return false;
+                }
+               
             }
+             else if(!st.empty() && ch=='c' ){
+               if(st.top()=='b'){
+                st.pop();
+                if(st.top()=='a') st.pop();
+                else return false;
+               }
+               else {return false;}
+             }
         }
+          
+
         return st.empty();
     }
-    // m2
-    // bool isValid(string s) {
-    //     if(s.size()==0) return true;
 
-    //     int find=s.find("abc");
-    //     if(find!=string::npos){ //found
-    //         string left=s.substr(0,find);
-    //          string right=s.substr(find+3,s.size());
-    //          return isValid(left+right);
+    //   bool isValid(string s) {
+    //     if (s[0] == 'b' || s[0] == 'c')
+    //         return false;
+
+    //     stack<char> st;
+
+    //     for (char ch : s) {
+    //         if (ch == 'a') {
+    //             st.push('a');
+    //              }
+    //         else if (ch == 'b'){
+    //             if (!st.empty() && st.top() == 'a') st.push('b');
+    //             else return false;
+    //         } else if (ch == 'c') { // it will be c
+    //             if (!st.empty() && st.top() == 'b') {
+    //                 st.pop();
+    //                 if (!st.empty() && st.top() == 'a') {
+    //                     st.pop();
+    //                 } else
+    //                     return false;
+    //             } else
+    //                 return false;
+    //         }
     //     }
-    //     return false;
-    // }
+    //     return st.empty();
 };
