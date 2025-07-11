ce3b223c239633a49c29ee46685d152d08743b33
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-12 01:48:27 +0530

    Time: 0 ms (100%), Space: 12.8 MB (60.16%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -5,16 +5,11 @@ public:
 
     // Use nums1 to ask the query:
     // What's the next greater element of nums1[i] in nums2
-
+   
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> m;
         stack<int> st;
         for (int i = nums2.size() - 1; i >= 0; i--) {
-
-            if (st.empty()) {
-                m[nums2[i]] = -1;
-                st.push(nums2[i]);
-            } else {
 
                 while (!st.empty() && st.top() <= nums2[i]) {
                     st.pop();
@@ -25,7 +20,7 @@ public:
                     m[nums2[i]] = st.top();
                 }
                 st.push(nums2[i]);
-            }
+            
         }
         vector<int> ans;
 
@@ -34,4 +29,34 @@ public:
         }
         return ans;
     }
+
+
+    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    //     unordered_map<int, int> m;
+    //     stack<int> st;
+    //     for (int i = nums2.size() - 1; i >= 0; i--) {
+
+    //         if (st.empty()) {
+    //             m[nums2[i]] = -1;
+    //             st.push(nums2[i]);
+    //         } else {
+
+    //             while (!st.empty() && st.top() <= nums2[i]) {
+    //                 st.pop();
+    //             }
+    //             if (st.empty()) {
+    //                 m[nums2[i]] = -1;
+    //             } else {
+    //                 m[nums2[i]] = st.top();
+    //             }
+    //             st.push(nums2[i]);
+    //         }
+    //     }
+    //     vector<int> ans;
+
+    //     for (auto it : nums1) {
+    //         ans.push_back(m[it]);
+    //     }
+    //     return ans;
+    // }
 };
