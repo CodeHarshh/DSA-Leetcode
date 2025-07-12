ecc3910fa3b8571089243440fe576d0f6823146f
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-13 00:00:14 +0530

    Time: 0 ms (100%), Space: 24.5 MB (9.4%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,35 +1,23 @@
 class MinStack {
 public:
-      vector<pair<int,int>>v;
-    MinStack() {}
-    
-    void push(int val) {
-      if(v.empty()) v.push_back({val,val});
-      else{
-        int curr=min(val,v.back().second);
-         v.push_back({val,curr});
-      }
-    
-    }
-    
-    void pop() {
-        v.pop_back();
-    }
-    
-    int top() {
-        return v.back().first;
-    }
-    
-    int getMin() {
-       return v.back().second; 
-    }
-};
+    stack<pair<int, int>> st; // val,smallest
+    MinStack() {}             // initializes the stack object.
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+    void push(int val) {
+        if (st.empty()) {
+            st.push({val, val});
+        } else {
+            if (st.top().second > val) {
+                st.push({val, val});
+            } else {
+                st.push({val, st.top().second});
+            }
+        }
+    }
+
+    void pop() { st.pop(); }
+
+    int top() { return st.top().first; }
+
+    int getMin() { return st.top().second; }
+};
