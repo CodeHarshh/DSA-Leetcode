136fce00f8db4f6f06a0d6d733bd497e4d3832f2
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-15 04:14:03 +0530

    Time: 40 ms (9.42%), Space: 107.4 MB (25.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    // \U0001f539 Short Logic Summary
+
+    // Use a stack to keep indices of temperatures in decreasing order.
+    // For each day i, check if current temp is higher than the top of stack.
+    // If yes, pop and calculate how many days it took to get a warmer day.
+    // Push current day i onto the stack.
+
     vector<int> dailyTemperatures(vector<int>& temp) {
         vector<int> ans(temp.size(), 0);
         stack<int> st;
 
         for (int i = 0; i < temp.size(); i++) {
+
             while (!st.empty() && temp[i] > temp[st.top()]) {
                 ans[st.top()] = i - st.top();
                 st.pop();
@@ -13,4 +21,18 @@ public:
         }
         return ans;
     }
+
+    // vector<int> dailyTemperatures(vector<int>& temp) {
+    //     vector<int> ans(temp.size(), 0);
+    //     stack<int> st;
+
+    //     for (int i = 0; i < temp.size(); i++) {
+    //         while (!st.empty() && temp[i] > temp[st.top()]) {
+    //             ans[st.top()] = i - st.top();
+    //             st.pop();
+    //         }
+    //         st.push(i);
+    //     }
+    //     return ans;
+    // }
 };
