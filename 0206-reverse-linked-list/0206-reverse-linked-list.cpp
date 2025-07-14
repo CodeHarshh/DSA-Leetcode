cc88e031c69bb3826151fb921dfecbcfa8126d53
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-14 22:24:40 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.08%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,47 +1,14 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-
-
-// class Solution {
-// public:
-//     ListNode* reverseList(ListNode*& prev, ListNode*& curr) {
-//         if (curr == NULL)
-//             return prev;
-//         ListNode* forward = curr->next;
-//         curr->next = prev;
-//         prev = curr;
-//         curr = forward;
-//         reverseList(prev, curr);
-//         return prev;
-//     }
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode* prev = NULL;
-//         ListNode* curr = head;
-//         ListNode* newhead = reverseList(prev, curr);
-//         return newhead;
-//     }
-// };
-
-
 
 class Solution {
 public:
     ListNode* reverseList(ListNode*& prev, ListNode*& curr) {
-      if(curr==NULL) return NULL;
-      ListNode* forward=curr->next;
-      curr->next=prev;
-      prev=curr;
-      reverseList(prev,forward);
-      return prev;
+     if(curr==NULL)return prev;
+     ListNode* next=curr->next;
+     curr->next=prev;
+     prev=curr;
+     curr=next;
+     return reverseList(prev,curr);
+
     }
     ListNode* reverseList(ListNode* head) {
       ListNode* prev=NULL;
