eb374e4f088f87ce230e1c58fe955269f98c6fb2
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-16 02:25:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (69.32%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    void solve(string& s, string& part) {
 
-        if (s.find(part) == -1) {
-            return;
-        }
-        if (s.find(part) != -1) {
-            s.erase(s.find(part), part.length());
-        }
-
-        solve(s, part);
-    }
-
-    string removeOccurrences(string s, string part) {
-
-        // m1
-        //  while(s.find(part)!=-1){
-        //    s.erase(s.find(part),part.length());
-        //  }
-        //  return s;
-
-        solve(s, part);
+string removeOccurrences(string s, string part) {
+ while(s.find(part)!=-1){
+    s.erase(s.find(part),part.length());
+ }
         return s;
-    }
+}
+
+
+
+
+
+    // void solve(string& s, string& part) {
+
+    //     if (s.find(part) == -1) {
+    //         return;
+    //     }
+    //     if (s.find(part) != -1) {
+    //         s.erase(s.find(part), part.length());
+    //     }
+
+    //     solve(s, part);
+    // }
+
+    // string removeOccurrences(string s, string part) {
+
+    //     // m1
+    //     //  while(s.find(part)!=-1){
+    //     //    s.erase(s.find(part),part.length());
+    //     //  }
+    //     //  return s;
+
+    //     solve(s, part);
+    //     return s;
+    // }
 };
