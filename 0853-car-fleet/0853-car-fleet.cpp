138bb45eba9177447eb4057bbaf0f0f34120c775
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-16 00:35:36 +0530

    Time: 31 ms (85.19%), Space: 82.4 MB (77.76%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,31 +1,61 @@
 class Solution {
-    // we can also use vector<pair> intead of
-    class Car {
     public:
-        int pos, speed;
-        Car(int p, int s) : pos(p), speed(s){};
-    };
-    static bool mycomp(Car& a, Car& b) { return a.pos < b.pos; }
+//     // we can also use vector<pair> intead of
+//     class Car {
+//     public:
+//         int pos, speed;
+//         Car(int p, int s) : pos(p), speed(s){};
+//     };
+//     static bool mycomp(Car& a, Car& b) { return a.pos < b.pos; }
 
-public:
+// public:
+//     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+//         vector<Car> cars;
+//         for (int i = 0; i < position.size(); i++) {
+//             Car car(position[i], speed[i]);
+//             cars.push_back(car);
+//         }
+//         sort(cars.begin(), cars.end(), mycomp);
+
+//         stack<float> st;
+
+//         for (auto it : cars) {
+//             float time = (float)(target - it.pos) / it.speed;
+//             // time >> so it will take more time to
+//             while (!st.empty() && time >= st.top()) {
+//                 st.pop();
+//             }
+//             st.push(time);
+//         }
+//         return st.size();
+//     }
+
+
+
+
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<Car> cars;
-        for (int i = 0; i < position.size(); i++) {
-            Car car(position[i], speed[i]);
-            cars.push_back(car);
-        }
-        sort(cars.begin(), cars.end(), mycomp);
+        stack<float>fleets;
+        vector<pair<int,float>>v;
 
-        stack<float> st;
-
-        for (auto it : cars) {
-            float time = (float)(target - it.pos) / it.speed;
-            // time >> so it will take more time to
-            while (!st.empty() && time >= st.top()) {
-                st.pop();
-            }
-            st.push(time);
+        for(int i=0;i<position.size();i++){
+          v.push_back({position[i], float(target - position[i]) / speed[i]}); // (postion , time to reach)
         }
-        return st.size();
+
+       // Sort by position descending using rbegin/rend
+        sort(v.begin(), v.end());
+
+        for(int i=0;i<position.size();i++){
+          float temp=v[i].second;   // time to reach to destination
+
+          while(!fleets.empty() && temp>=fleets.top()){
+            fleets.pop();
+          }      
+
+          fleets.push(temp);
+
+         }
+
+    return fleets.size();
+
     }
 };
