7981d979af37923ad2e83e8d5b03fcba4442269b
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-16 04:22:47 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.69%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -2,32 +2,64 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if ((head && head->next == NULL) && n == 1)
-            return NULL;
-        if (!head || !head->next)
-            return head;
+        if (!head )return head;
+
+        int cnt = 0;
         ListNode* temp = head;
         ListNode* prev = NULL;
-        int cnt = 0;
         while (temp) {
             cnt++;
             temp = temp->next;
         }
-        int pos = cnt - n + 1;
-        temp = head;
-        for (int i = 1; i < pos; i++) {
-            prev = temp;
-            temp = temp->next;
-        }
+       
+        int pos=cnt-n+1;
+        temp=head;
 
-        if (prev == NULL) {
-            head = head->next;
-            return head;
-        } else {
-            prev->next = prev->next->next;
-            return head;
-        }
-
-        return head;
+       if (pos == 1) {
+        return head->next;
     }
+
+
+        for(int i=1;i<pos && temp!=NULL ;i++){
+            prev=temp;
+            temp=temp->next;
+        }
+
+        
+    prev->next=temp->next;
+       delete temp;
+       return head;
+
+    }
+
+    // ListNode* removeNthFromEnd(ListNode* head, int n) {
+    //     if ((head && head->next == NULL) && n == 1)
+    //         return NULL;
+    //     if (!head || !head->next)
+    //         return head;
+    //     ListNode* temp = head;
+    //     ListNode* prev = NULL;
+    //     int cnt = 0;
+    //     while (temp) {
+    //         cnt++;
+    //         temp = temp->next;
+    //     }
+    //     int pos = cnt - n + 1;
+    //     temp = head;
+    //     for (int i = 1; i < pos; i++) {
+    //         prev = temp;
+    //         temp = temp->next;
+    //     }
+    //   //If prev is NULL, it means that the node to be removed is the head
+    //   node
+    //     if (prev == NULL) {
+    //         head = head->next;
+    //         return head;
+    //     } else {
+    //         prev->next = prev->next->next;
+    //         return head;
+    //     }
+
+    //     return head;
+    // }
 };
