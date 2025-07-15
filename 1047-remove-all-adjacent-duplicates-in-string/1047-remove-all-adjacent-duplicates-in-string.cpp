350cdbf9c2676b0a13b8bdcb760d475a69d5c329
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-16 02:22:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
-   string removeDuplicates(string s) {
+
+ string removeDuplicates(string s) {
    string ans="";
-  
+
+
+
    for(int i=0;i<s.size();i++){
-     char current_elem=s[i];
-    if(ans.empty()) ans.push_back(current_elem);
-    else if (!ans.empty() && ans.back()==current_elem) ans.pop_back();
-    else if (!ans.empty() && ans.back()!=current_elem) ans.push_back(current_elem);
+    if(ans.empty()){
+        ans=ans+s[i];
+    }
+    else if(!ans.empty() && ans.back()==s[i]){
+        ans.pop_back();
+    }else{
+          ans=ans+s[i];
+    }
    }
    return ans;
    }
+
+
+
+
+
+
+
+//    string removeDuplicates(string s) {
+//    string ans="";
+  
+//    for(int i=0;i<s.size();i++){
+//      char current_elem=s[i];
+//     if(ans.empty()) ans.push_back(current_elem);
+//     else if (!ans.empty() && ans.back()==current_elem) ans.pop_back();
+//     else if (!ans.empty() && ans.back()!=current_elem) ans.push_back(current_elem);
+//    }
+//    return ans;
+//    }
 };
