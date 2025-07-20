ee93d9b1fe39d2da3986592dfb593f214fb6897a
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-21 04:11:08 +0530

    Time: 0 ms (100%), Space: 9.9 MB (90.32%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
     string reverseWords(string s) {
+
         reverse(s.begin(), s.end());
         int left = 0, right = 0;
-        int i = 0;
-        int n = s.size();
+        int i = 0, n = s.size();
+
         while (i < n) {
 
-            while (i < n && s[i] == ' ')
+            while (i < n && s[i] == ' ') {
                 i++;
-            if (i == n)
-                break;
+            }
+            if (i >= n) break;
 
             while (i < n && s[i] != ' ') {
                 s[right] = s[i];
-                right++;
                 i++;
+                right++;
             }
+
             reverse(s.begin() + left, s.begin() + right);
-            // now add space after reverse
-            s[right++] = ' ';
-            left = right;
+
             i++;
+            s[right] = ' ';
+            right++;
+            left = right;
+           
         }
+         cout<<right;
+
         s.resize(right - 1);
         return s;
     }
+
+    // string reverseWords(string s) {
+    //     reverse(s.begin(), s.end());
+    //     int left = 0, right = 0;
+    //     int i = 0;
+    //     int n = s.size();
+    //     while (i < n) {
+
+    //         while (i < n && s[i] == ' ') i++;
+
+    //         if (i == n)
+    //             break;
+
+    //         while (i < n && s[i] != ' ') {
+    //             s[right] = s[i];
+    //             right++;
+    //             i++;
+    //         }
+    //         reverse(s.begin() + left, s.begin() + right);
+    //         // now add space after reverse
+    //         s[right++] = ' ';
+    //         left = right;
+    //         i++;
+    //     }
+    //     s.resize(right - 1);
+    //     return s;
+    // }
 };
