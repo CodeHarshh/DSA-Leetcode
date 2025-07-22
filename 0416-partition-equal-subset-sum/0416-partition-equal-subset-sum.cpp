8498cb6a4cd1a5f78f7b55b7987663c0596a3fba
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-23 00:56:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
 
-     // TLE
- // bool solve(vector<int>& nums, int target, int i) {
-    //     if (target < 0 || i == nums.size())
-    //         return false;
-    //     if (target == 0)
-    //         return true;
+    //  TLE
+//  bool solve(vector<int>& nums, int target, int i) {
+//         if (target < 0 || i == nums.size())
+//             return false;
+//         if (target == 0)
+//             return true;
 
-    //     bool inc = solve(nums, target - nums[i], i + 1);
-    //     bool exc = solve(nums, target, i + 1);
-    //     return inc || exc;
-    // }
+//         bool inc = solve(nums, target - nums[i], i + 1);
+//         bool exc = solve(nums, target, i + 1);
+//         return inc || exc;
+//     }
 
-    // bool canPartition(vector<int>& nums) {
-    //     int sum = accumulate(nums.begin(), nums.end(),0);
-    //     if (sum % 2 != 0)
-    //         return false;
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(), nums.end(),0);
+//         if (sum % 2 != 0)
+//             return false;
 
-    //     int target = sum / 2;
-    //     return solve(nums,target,0);
-    // }
+//         int target = sum / 2;
+//         return solve(nums,target,0);
+//     }
+
+
+
+ bool solve(vector<int>& nums, int target, int i) {
+    if(target==0) return true;
+    if(target<0) return false;
+    if(i>=nums.size()) return false;
+     
+    //  if(nums[i<=target]){
+        bool inc=solve(nums,target-nums[i],i+1);
+    //  }
+    
+    bool exc=solve(nums,target,i+1);
+
+    return inc||exc;
+    }
+
+    bool canPartition(vector<int>& nums) {
+       int sum=0;
+       for(int i=0;i<nums.size();i++){
+        sum=sum+nums[i];
+       }
+       if(sum%2!=0) return false;
+       int target=sum/2;
+      return solve(nums,target,0);
+    }
+
+
+
 
 
 
@@ -90,38 +119,38 @@ public:
 
 // mehtod 3 (space optimization)
 
-bool Optimization(vector<int>& nums, int target, int i) {
-  int n=nums.size();
-    vector<int>curr(target+1,0);
-    vector<int>next(target+1,0);
+// bool Optimization(vector<int>& nums, int target, int i) {
+//   int n=nums.size();
+//     vector<int>curr(target+1,0);
+//     vector<int>next(target+1,0);
 
-        for(int col=0;col<=nums.size();col++){ // if (target == 0) return true;  zero th row me sb ko true set krna ha;
-            next[0]=true;
-        }
+//         for(int col=0;col<=nums.size();col++){ // if (target == 0) return true;  zero th row me sb ko true set krna ha;
+//             next[0]=true;
+//         }
 
     
-        for (int i = n - 1; i >= 0; i--) {
-             for (int t = 1; t <= target; t++) {
-           bool inc = 0;
-            if(t - nums[i] >=0){
-                inc=next[t - nums[i]];
-            }
-        bool exc = next[t];
-        curr[t]=inc || exc;
+//         for (int i = n - 1; i >= 0; i--) {
+//              for (int t = 1; t <= target; t++) {
+//            bool inc = 0;
+//             if(t - nums[i] >=0){
+//                 inc=next[t - nums[i]];
+//             }
+//         bool exc = next[t];
+//         curr[t]=inc || exc;
        
 
-            } next=curr;
-        }
+//             } next=curr;
+//         }
         
-        return next[target];
-    }
+//         return next[target];
+//     }
 
-    bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(),0);
-        if (sum % 2 != 0) return false;
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(), nums.end(),0);
+//         if (sum % 2 != 0) return false;
       
-        int target = sum / 2;
+//         int target = sum / 2;
        
-        return Optimization(nums,target,0);
-    }
+//         return Optimization(nums,target,0);
+//     }
 };
