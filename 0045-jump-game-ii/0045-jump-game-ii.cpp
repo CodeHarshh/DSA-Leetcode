1defc134bb0f5282fa0aebe615d77dce6445cfc3
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-23 23:32:06 +0530

    Time: 251 ms (5.01%), Space: 22.6 MB (5.25%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    // int solve(vector<int>& nums, int n, int i, int cnt) {
-    //     if (i >= n)
-    //         return cnt;
-    //     if (nums[1] == 0)
-    //         return INT_MAX;
+    int issolve(vector<int>& nums, int n, int i,
+                vector<int>&dp) {
+        if (i > n) return INT_MAX;
+        if (i >= n) return 0;
+        if (nums[i] == 0) return INT_MAX;             
+        if (dp[i] != -1) return dp[i];
 
-    //     int min_jump = INT_MAX;
+        int minJump = INT_MAX;
 
-    //     // j== jump
-    //     for (int j = 1; j <= nums[i]; j++) {
-    //         int ans = solve(nums, n, i + j, cnt + 1);
-    //         if (ans != INT_MAX) {
-    //             min_jump = min(min_jump, ans);
-    //         }
-    //     }
-    //     return min_jump;
-    // }
-
-    int jump(vector<int>& nums) {
- int jumps = 0;
-        int currentEnd = 0;
-        int furthest = 0;
-
-        for (int i = 0; i < nums.size() - 1; ++i) { // O(n) loop
-            // Update the furthest point we can reach
-            furthest = std::max(furthest, i + nums[i]);
-
-            // If we have reached the end of the current range
-            if (i == currentEnd) {
-                jumps++;
-                currentEnd = furthest;
-
-                // If the current end reaches or exceeds the last index
-                if (currentEnd >= nums.size() - 1) {
-                    break; // Exit the loop early if end is reached
-                }
+        for (int j = 1; j <= nums[i]; j++) {
+            int recAns = issolve(nums, n, i + j, dp);
+            if (recAns != INT_MAX) {
+                minJump = min(minJump, 1+ recAns);
             }
         }
 
-        return jumps;
+        dp[i] = minJump;
+        return dp[i];
+    }
 
-        // int cnt = 0;
-        // int n = nums.size() - 1;
-        // return solve(nums, n, 0, cnt);
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size() - 1;
+        vector<int>dp(n+1,-1);
+         return issolve(nums, n, 0, dp);
     }
 };
