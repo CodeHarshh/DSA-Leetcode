d790917cebc65dcb545dcc645622c978b69edff9
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-25 02:46:46 +0530

    Time: 0 ms (100%), Space: 9.9 MB (89.24%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,18 +1,35 @@
 
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
 
-        ListNode* fast = head;
-        ListNode* slow = head;
 
-        while(fast->next && fast->next->next){
-            fast=fast->next->next;
-            slow=slow->next;
-        }
-        if(fast->next){
-            slow=slow->next;
-        }
-        return slow;
+  ListNode* middleNode(ListNode* head) {
+    ListNode* fast = head;
+    ListNode* slow = head;
+
+      while(fast&&fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+      }
+      return slow;
     }
+
+
+
+
+
+    // ListNode* middleNode(ListNode* head) {
+
+    //     ListNode* fast = head;
+    //     ListNode* slow = head;
+
+    //     while(fast->next && fast->next->next){
+    //         fast=fast->next->next;
+    //         slow=slow->next;
+    //     }
+    //     if(fast->next){  // we added this line because if it had two middle elements
+    //         slow=slow->next;
+    //     }
+    //     return slow;
+    // }
 };
