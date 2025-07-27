388afcad19e2406b673a7a30b91b2f33de6575f6
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-07-27 23:25:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (87.04%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,6 +1,21 @@
 
 class Solution {
 public:
+
+  void solve (TreeNode* root,  vector<int>&ans){
+    if(root==NULL) return ;
+
+    solve(root->left,ans);
+    ans.push_back(root->val);
+    solve(root->right,ans);
+ }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+    }
+
+
     // tc->o(N) , sc->o(N)
     // void solve(TreeNode* root, vector<int>& ans) {
     //     if (root == NULL)
@@ -21,34 +36,34 @@ public:
 
     // by using morries traversal tc->o(N) , sc->o(1)
 
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        TreeNode* curr = root;
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     TreeNode* curr = root;
 
-        while (curr) {
-            // left node is null , then visit it and go write
+    //     while (curr) {
+    //         // left node is null , then visit it and go write
+    //            // find inorder predecessor
+    //             // ek baar left then right right right
+    //         if (curr->left == NULL) {
+    //             ans.push_back(curr->val);
+    //             curr=curr->right;
+    //         } else {
+    //             TreeNode* pred = curr->left;
 
-            if (curr->left == NULL) {
-                ans.push_back(curr->val);
-                curr=curr->right;
-            } else { // find inorder predecessor
-                // ek baar left then right right right..
-                TreeNode* pred = curr->left;
+    //              while (pred->right != NULL && pred->right != curr) {
+    //             pred = pred->right;
+    //         }
 
-                 while (pred->right != NULL && pred->right != curr) {
-                pred = pred->right;
-            }
-
-                if (pred->right == NULL) {
-                    pred->right = curr;
-                    curr = curr->left;
-                } else {
-                    pred->right = NULL;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-            }
-        }
-        return ans;
-    }
+    //             if (pred->right == NULL) {
+    //                 pred->right = curr;
+    //                 curr = curr->left;
+    //             } else {
+    //                 pred->right = NULL;
+    //                 ans.push_back(curr->val);
+    //                 curr = curr->right;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
