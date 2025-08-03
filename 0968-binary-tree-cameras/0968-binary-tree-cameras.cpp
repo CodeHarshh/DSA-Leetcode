e500fe82b7c2ff405c802d3bc36af5456cc73215
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-04 02:30:37 +0530

    Time: 0 ms (100%), Space: 21.4 MB (68.51%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -7,36 +7,16 @@ public:
     // 2->i have cam
 
     int solve(TreeNode* root, int& cam) {
-        if (!root) return 1;
-
-        if (!root->left && !root->right)
-            return 1;
+        if (!root)return 1;
 
         int leftside = solve(root->left, cam);
         int rightside = solve(root->right, cam);
 
-        if (leftside == 1 && rightside == 1) return 0;
-        if (leftside == 0 && rightside == 0) {
+        if (leftside == 0 || rightside == 0) {
             cam++;
             return 2;
         }
-        if (leftside == 2 && rightside == 2) return 1;
-
-        if (leftside == 2 && rightside == 0) {
-           cam++;
-            return 2;
-        }
-        if (leftside == 0 && rightside == 2) {
-            cam++;
-            return 2;
-        }
-        if (leftside == 1 && rightside == 0) {
-            return 2;
-        }
-        if (leftside == 0 && rightside == 1) {
-            cam++;
-            return 2;
-        }
+        if (leftside == 2 || rightside == 2) return 1;
 
         return 0;
     }
@@ -44,8 +24,9 @@ public:
     int minCameraCover(TreeNode* root) {
         int cam = 0;
         if (!root)
-            return cam;
-        solve(root, cam);
+            return 0;
+        if (solve(root, cam) == 0)
+            cam++;
 
         return cam;
     }
