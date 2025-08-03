71f70d2a0a14f0b9ba8b617c7296cd86f7cb8999
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-03 23:20:59 +0530

    Time: 0 ms (100%), Space: 19.8 MB (10.27%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,36 +1,83 @@
 
 class Solution {
 public:
+    //   ✅ Width of Binary Tree – Summary
+    // Use level order traversal (BFS) with a queue storing {node, index}
+    // (starting index = 1). For each level, record the first and last indices,
+    // then compute: width = last - first + 1. Run a for loop from 0 to size -
+    // 1, pop each node, and: Push left child with index 2 * i Push right child
+    // with index 2 * i + 1 Always push children at the back of the queue. After
+    // each level, update maxWidth if the current width is larger.
+
     int widthOfBinaryTree(TreeNode* root) {
 
-         unsigned long long  maxwidth = 0;
-        if (!root)return maxwidth;
+         unsigned long long maxwidth = 0;
+        if (!root)
+            return maxwidth;
 
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 1});
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
 
         while (!q.empty()) {
-           unsigned long long  size = q.size();
-           unsigned   long long  s = q.front().second;
-             unsigned long long  e = q.back().second;
+            int size = q.size();
 
-            maxwidth = max(maxwidth, e - s + 1);
+             unsigned long long front = q.front().second;
+             unsigned long long back = q.back().second;
+             unsigned long long width = back - front + 1;
 
+            maxwidth = max(maxwidth, width);
             for (int i = 0; i < size; i++) {
-                auto it = q.front();
+                  auto it = q.front();
+                  q.pop();
+                  
+                  TreeNode* node=it.first;
+                   unsigned long long indx=it.second;
 
-                unsigned long long  indx = it.second;
-                q.pop();
+                  if(node->left){
+                    q.push({node->left,2*indx+1});
+                  }
 
-                if (it.first->left != NULL) {
-                    q.push({it.first->left, 2 * indx});
-                }
-                if (it.first->right != NULL) {
-                    q.push({it.first->right, 2 * indx + 1});
-                }
+                  if(node->right){
+                    q.push({node->right,2*indx+2});
+                  }  
             }
-        }
-
-        return maxwidth;
+  
+         }
+     return maxwidth;
     }
+
+    
+
+    // int widthOfBinaryTree(TreeNode* root) {
+
+    //      unsigned long long  maxwidth = 0;
+    //     if (!root)return maxwidth;
+
+    //     queue<pair<TreeNode*, unsigned long long>> q;
+    //     q.push({root, 1});
+
+    //     while (!q.empty()) {
+    //        unsigned long long  size = q.size();
+    //        unsigned   long long  s = q.front().second;
+    //          unsigned long long  e = q.back().second;
+
+    //         maxwidth = max(maxwidth, e - s + 1);
+
+    //         for (int i = 0; i < size; i++) {
+    //             auto it = q.front();
+
+    //             unsigned long long  indx = it.second;
+    //             q.pop();
+
+    //             if (it.first->left != NULL) {
+    //                 q.push({it.first->left, 2 * indx});
+    //             }
+    //             if (it.first->right != NULL) {
+    //                 q.push({it.first->right, 2 * indx + 1});
+    //             }
+    //         }
+    //     }
+
+    //     return maxwidth;
+    // }
 };
