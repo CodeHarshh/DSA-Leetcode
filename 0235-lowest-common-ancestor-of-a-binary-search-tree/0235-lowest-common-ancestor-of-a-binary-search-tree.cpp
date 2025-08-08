03aaba63f6f420d48cb3ac7084c993f6eb9c309e
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-08 12:18:17 +0530

    Time: 23 ms (53.82%), Space: 23.3 MB (99.2%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,26 +1,44 @@
 
-
 class Solution {
 public:
-    // solve(TreeNode* root, TreeNode* p, TreeNode* q) {}
+    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return NULL;
+        int curr = root->val;
+        if (curr < p->val && curr < q->val) {
+            return solve(root->right, p, q);
+        }
+        if (curr > p->val && curr > q->val) {
+            return solve(root->left, p, q);
+        }
+        return root;
+    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root) return NULL;
-        if(root== p || root==q) return root;
-
-        if (p->val < root->val && q->val < root->val)
-            lowestCommonAncestor(root->left, p, q);
-        else if (p->val > root->val && q->val > root->val)
-            lowestCommonAncestor(root->right, p, q);
-
-    
-            TreeNode* leftside=lowestCommonAncestor(root->left,p,q);
-            TreeNode* rightside=lowestCommonAncestor(root->right,p,q);
-
-            if(leftside && rightside) return root;
-            // this line just to return something //we can also use 
-            // return leftside ? leftside : rightside;
-
-             return rightside ? rightside : leftside;
+        return solve(root, p, q);
     }
 };
+
+// class Solution {
+// public:
+//     // solve(TreeNode* root, TreeNode* p, TreeNode* q) {}
+
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+//     {
+//         if (!root) return NULL;
+//         if(root== p || root==q) return root;
+
+//         if (p->val < root->val && q->val < root->val)
+//             lowestCommonAncestor(root->left, p, q);
+//         else if (p->val > root->val && q->val > root->val)
+//             lowestCommonAncestor(root->right, p, q);
+
+//             TreeNode* leftside=lowestCommonAncestor(root->left,p,q);
+//             TreeNode* rightside=lowestCommonAncestor(root->right,p,q);
+
+//             if(leftside && rightside) return root;
+//             // this line just to return something //we can also use
+//             // return leftside ? leftside : rightside;
+
+//              return rightside ? rightside : leftside;
+//     }
+// };
