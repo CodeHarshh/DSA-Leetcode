8f77695a7c1e6f58b57580d1ee1b7dc56770ea56
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-13 23:13:12 +0530

    Time: 0 ms (100%), Space: 61 MB (19.56%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -2,34 +2,72 @@
 class Solution {
 public:
     // only two nodes are swapped by mistake
+
     TreeNode* prev;
     TreeNode* first;
     TreeNode* middle;
     TreeNode* last;
 
     void inorder(TreeNode*& root) {
-        if (!root)return;
-        inorder(root->left);
+        if(!root)return;
 
-        if (prev != NULL && (root->val < prev->val)) { //  voilation occurs
-            if (first == NULL) {                       // first time voilation
-                first = prev;
-                middle = root;
-            } else { // second time voilation
-                last = root;
+        inorder(root->left);
+        if(prev!=NULL && prev->val > root->val){
+            if(first==NULL){ // 1 st time violation;
+             first=prev;
+             middle=root;
             }
+            else{
+                last=root;
+            } 
         }
-        prev = root;
+         prev=root;
         inorder(root->right);
+        
     }
 
     void recoverTree(TreeNode* root) {
-        first = middle = last = NULL;
-        prev = new TreeNode(INT_MIN);
-        inorder(root);
-        if (first && last)
-            swap(first->val, last->val);
-        else if (first && middle)
-            swap(first->val, middle->val);
+         prev=first=middle=last=NULL;
+         inorder(root);
+         if(first!=NULL && last!=NULL){
+            swap(first->val,last->val);
+         }
+         else if(first!=NULL && middle!=NULL){
+            swap(first->val,middle->val);
+         }     
+
+      return;
     }
+
+
+    // TreeNode* prev;
+    // TreeNode* first;
+    // TreeNode* middle;
+    // TreeNode* last;
+
+    // void inorder(TreeNode*& root) {
+    //     if (!root)return;
+    //     inorder(root->left);
+
+    //     if (prev != NULL && (root->val < prev->val)) { //  voilation occurs
+    //         if (first == NULL) {                       // first time voilation
+    //             first = prev;
+    //             middle = root;
+    //         } else { // second time voilation
+    //             last = root;
+    //         }
+    //     }
+    //     prev = root;
+    //     inorder(root->right);
+    // }
+
+    // void recoverTree(TreeNode* root) {
+    //     first = middle = last = NULL;
+    //     prev = new TreeNode(INT_MIN);
+    //     inorder(root);
+    //     if (first && last) // if 2 time voilation occur
+    //         swap(first->val, last->val);
+    //     else if (first && middle) // if only  one time violation occurs
+    //         swap(first->val, middle->val);
+    // }
 };
