d4c810418cda5a81c105d09eb9bc41c466ad9254
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-20 12:58:50 +0530

    Time: 16 ms (92.98%), Space: 61.6 MB (45.92%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // solving by min heap
+        
+     // min heap
 
-        priority_queue<int, vector<int>, greater<int>> pq;
-        // push initial k elements
-
-        for (int i = 0; i < k; i++) {
+     priority_queue<int ,vector<int>,greater<int>>pq;
+     
+     for(int i=0;i<k;i++){
+        pq.push(nums[i]);
+     }
+     for(int i=k;i<nums.size();i++){
+        if(pq.top()<nums[i]){
+            pq.pop();
             pq.push(nums[i]);
         }
+     }
 
-        for (int i = k; i < nums.size(); i++) {
-            if (pq.top() < nums[i]) {
-                pq.pop();
-                pq.push(nums[i]);
-            }
-        }
-    return pq.top();
+     return pq.top();
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         // solving by min heap
+
+//         priority_queue<int, vector<int>, greater<int>> pq;
+//         // push initial k elements
+
+//         for (int i = 0; i < k; i++) {
+//             pq.push(nums[i]);
+//         }                                                                                   
+
+//         for (int i = k; i < nums.size(); i++) {
+//             if (pq.top() < nums[i]) {
+//                 pq.pop();
+//                 pq.push(nums[i]);
+//             }
+//         }
+//     return pq.top();
+//     }
+// };
