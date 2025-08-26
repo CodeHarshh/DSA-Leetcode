d316da9cec8f8d3dd25d81f3283f759ffcf09bf9
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-26 22:55:11 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,31 +2,64 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int s = 0, e = nums.size() - 1;
-        int mid = s + (e - s) / 2;
+        int mid;
 
         while (s <= e) {
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-            
-            if (nums[s] <= nums[mid]) {
-                if (nums[s] <= target && target <= nums[mid]) {
-                e = mid - 1;
-                } else {
-                   s = mid + 1;
-               } }
-            
-            else {
-                if (nums[mid] <= target && target <= nums[e]) {
-                    s = mid + 1;
-                } else {
-                    e = mid - 1;
-                }
-            }
-            
             mid = s + (e - s) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[s] <= nums[mid]) { // check whether mid lies in 1st or 2nd half (1st half here)
+                if (nums[s] <= target && target< nums[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+
+            } else {
+
+                if(nums[mid]<=target && target<=nums[e]){                                        
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+
+            }
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int s = 0, e = nums.size() - 1;
+//         int mid = s + (e - s) / 2;
+
+//         while (s <= e) {
+
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
+
+//             if (nums[s] <= nums[mid]) {
+//                 if (nums[s] <= target && target <= nums[mid]) {
+//                 e = mid - 1;
+//                 } else {
+//                    s = mid + 1;
+//                } }
+
+//             else {
+//                 if (nums[mid] <= target && target <= nums[e]) {
+//                     s = mid + 1;
+//                 } else {
+//                     e = mid - 1;
+//                 }
+//             }
+
+//             mid = s + (e - s) / 2;
+//         }
+//         return -1;
+//     }
+// };
