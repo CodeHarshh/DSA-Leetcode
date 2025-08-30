f8eee02ea6ef9e696eeccfe583b00531790a3e78
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-08-30 22:20:51 +0530

    Time: 3 ms (50.29%), Space: 17.9 MB (80.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,62 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         vector<int> ans;
+//         unordered_map<int, int> m;
+//         for (int i = 0; i < nums.size(); i++) {
+//             m[nums[i]]++;
+//         }
+
+//         priority_queue<pair<int, int>, vector<pair<int, int>>,
+//                        greater<pair<int, int>>>pq;
+//         for (auto it : m) {
+//             // {freq,number}
+//             pq.push({it.second, it.first});
+//             if (pq.size() > k) {
+//                 pq.pop();
+//                } 
+//             }
+//         for (int i = 0; i < k; i++) {
+//             auto temp = pq.top();
+//             cout<<temp.second<<" ";
+//             pq.pop();
+//             ans.push_back(temp.second);
+//         }
+
+//         for(auto it:ans){
+//             // cout<<it<<" ";
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> ans;
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            m[nums[i]]++;
-        }
+       vector<int>ans;
+       unordered_map<int,int>m;
+       
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
 
-        priority_queue<pair<int, int>>pq;
+       for(int i=0;i<nums.size();i++){
+        m[nums[i]]++;
+       }
 
-
-//, vector<pair<int, int>>, greater<pair<int, int>>
-        for (auto it : m) {
-            // {freq,number}
-            pq.push({it.second, it.first});
-        }
-
-        for (int i = 0; i < k;i++) {
-            auto temp = pq.top();
+       for(auto it:m){//{freq,number}
+        pq.push({it.second,it.first});
+        if(pq.size()>k){
+            auto front=pq.top();
             pq.pop();
-            ans.push_back(temp.second);
         }
+       }
 
-        return ans;
+      while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+       
+       return ans;
     }
 };
