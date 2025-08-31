64d92cfd6ff7c204275c9134e97d64ff7269f37c
Harsh Kumar <152732598+CodeHarshh@users.noreply.github.com>
2025-09-01 01:27:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,13 +5,13 @@ public:
     double   pre = 1, suff = 1;
     double ans = INT_MIN;
     for (int i = 0; i < n; i++) {
-        if (pre == 0) pre = 1;
-        if (suff == 0) suff = 1;
+        // if (pre == 0) pre = 1;
+        // if (suff == 0) suff = 1;
         pre *= (double)nums[i];
         suff *= (double) nums[n - i - 1];
         ans = max(ans, max(pre, suff));
     }
-     return (int)ans;
+     return (int)ans;
 
 
   }
